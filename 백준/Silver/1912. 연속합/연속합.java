69490daafe395bdd79f1eee642e54187e653a0c2
Mingi Shin <96655921+ABCganada@users.noreply.github.com>
2023-12-22 17:38:46 +0900

    [Silver II] Title: 연속합, Time: 284 ms, Memory: 23816 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int[] arr;
+    static int n;
+
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+
+        n = Integer.parseInt(br.readLine());
+
+        arr = new int[n];
+        dp = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = arr[0];
+        int max = dp[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = arr[i];
+            int sum = dp[i - 1] + arr[i];
+
+            if (dp[i] < sum) {
+                dp[i] = sum;
+            }
+
+            if (dp[i] > max) {
+                max = dp[i];
+            }
+        }
+
+        System.out.println(max);
+    }
+}
