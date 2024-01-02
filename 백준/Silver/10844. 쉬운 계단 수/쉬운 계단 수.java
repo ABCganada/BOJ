9f9b561bb51e2edeb165a434a746403acbaa9c9b
Mingi Shin <96655921+ABCganada@users.noreply.github.com>
2024-01-02 20:07:37 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -10,7 +10,8 @@ public class Main {
 
     static int n;
     static long[][] dp;
-    static final long MOD = 1_000_000_000L;
+
+    static final int mod = 1000000000;
 
     public static void main(String[] args) throws IOException {
 
@@ -18,22 +19,22 @@ public class Main {
         dp = new long[n + 1][10];
 
         for (int i = 0; i < 10; i++) {
-            dp[1][i] = 1L;
+            dp[1][i] = 1;
         }
 
         for (int i = 2; i <= n; i++) {
-            dp[i][0] = dp[i-1][1] % MOD;
+            dp[i][0] = dp[i-1][1] % mod;
 
             for (int j = 1; j < 9; j++) {
-                dp[i][j] = (dp[i - 1][j - 1] % MOD + dp[i - 1][j + 1] % MOD) % MOD;
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
             }
 
-            dp[i][9] = dp[i - 1][8] % MOD;
+            dp[i][9] = dp[i - 1][8] % mod;
         }
 
         long cnt = 0;
         for (int i = 1; i < 10; i++) {
-            cnt = (cnt + dp[n][i]) % MOD;
+            cnt = (cnt + dp[n][i]) % mod;
         }
 
         System.out.println(cnt);
