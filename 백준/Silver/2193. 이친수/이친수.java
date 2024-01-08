12c52f9497fd2af92758e40f6fa0953ae7f03f36
Mingi Shin <96655921+ABCganada@users.noreply.github.com>
2024-01-08 22:49:09 +0900

    [Silver III] Title: 이친수, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int n;
+    static long[] dp = new long[91];
+
+    public static void main(String[] args) throws IOException {
+
+        n = Integer.parseInt(br.readLine());
+
+        dp[1] = 1L;
+        dp[2] = 1L;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[n]);
+    }
+}
