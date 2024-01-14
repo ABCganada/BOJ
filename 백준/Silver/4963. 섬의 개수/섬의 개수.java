e3788b37c30d358181e186e61e3590e82fb19e63
Mingi Shin <96655921+ABCganada@users.noreply.github.com>
2024-01-14 16:27:07 +0900

    [Silver II] Title: 섬의 개수, Time: 188 ms, Memory: 16328 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int w, h;
+    static int[][] map;
+    static boolean[][] visit;
+    static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+    static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+
+    static void dfs(int x, int y) {
+        visit[x][y] = true;
+
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && nx < h && ny >= 0 && ny < w) {
+                if (map[nx][ny] == 1 && !visit[nx][ny]) {
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+
+            if (w == 0 && h == 0) {
+                break;
+            }
+
+            map = new int[h][w];
+            visit = new boolean[h][w];
+
+            for (int i = 0; i < h; i++) {
+                st = new StringTokenizer(br.readLine());
+
+                for (int j = 0; j < w; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int cnt = 0;
+
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (map[i][j] == 1 && !visit[i][j]) {
+                        cnt++;
+                        dfs(i, j);
+                    }
+                }
+            }
+
+            sb.append(cnt + "\n");
+        }
+
+        System.out.println(sb);
+    }
+}
