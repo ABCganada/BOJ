31047aca50cd6d32bfe1e5bee053e320798f570a
Mingi Shin <96655921+ABCganada@users.noreply.github.com>
2024-07-12 22:20:38 +0900

    [level 2] Title: 카펫, Time: 11.15 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,15 +2,20 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = {0, 0};
         
-        for(int i=yellow; i>=1; i--){
+        for(int i=yellow; i>=1; i--){   //i = yellow의 가로 길이
+            
             if(yellow%i != 0){  //가로 세로 나누어 떨어짐 확인
                 continue;
             }
+            
+            
             int x = i + 2;  //가로
             int y = yellow/i + 2;   //세로
-            if(x < y){
-                break;
-            }
+            
+            // if(x < y){  //가로길이 보다 세로가 크면
+            //     break;
+            // }
+            
             // System.out.println(x + " " + y);
             
             if(x*y-yellow == brown){
